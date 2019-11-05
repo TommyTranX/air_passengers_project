09e0258a719ba4fb37993d31da82c42f8c2c3469
Ayam Tommy <tran.tommy@hotmail.com>
2019-11-05 08:56:52 +0100

    05/11/2019

--- submissions/starting_kit/regressor.py
+++ submissions/starting_kit/regressor.py
@@ -1,14 +1,22 @@
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.base import BaseEstimator
+from sklearn.model_selection import GridSearchCV
+from category_encoders import TargetEncoder
+from sklearn.neural_network import MLPRegressor
 
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.reg = RandomForestRegressor(
-            n_estimators=20, max_depth=50, max_features=10)
+        self.reg = MLPRegressor()
+        self.targ_enc = TargetEncoder(cols=['Departure','Arrival'], smoothing=8, min_samples_leaf=5)
+
+        
 
     def fit(self, X, y):
-        self.reg.fit(X, y)
-
+        self.targ_enc.fit(X, y)
+        X_train_te = self.targ_enc.transform(X.reset_index(drop=True))
+        self.reg.fit(X_train_te.values, y)
+        
     def predict(self, X):
-        return self.reg.predict(X)
+        X_train_te = self.targ_enc.transform(X.reset_index(drop=True))
+        return self.reg.predict(X_train_te.values)
